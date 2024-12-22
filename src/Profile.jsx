cbe31a2a8166679fea3d5ae7ae411dc8deb0927b
HarshavKoka <kokaharsha2203@gmail.com>
2024-12-22 23:13:08 +0530

    ok a final escapable change

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
     return (
         <>
             <h1>User</h1>
-            <h2>Hello,friends a very very warm to Welcome Username: {username}</h2>
+            <h2>Hello,friends a very very warm Welcome to so called Username: {username}</h2>
         </>
     );
 }
